22a218e71d67f79a82c1877f39aef2e1899de08f
LVQT <levietquocthinh@gmail.com>
2025-04-24 17:15:52 +0700

    Database successful connect

--- database/init.js
+++ database/init.js
@@ -1,10 +1,6 @@
 // src/database/init.js
 import sequelize from './db.js';
-import User from '../model/user.model.js';
-import product from '../model/product.model.js';
-import order from '../model/order.model.js';
-import orderDetails from '../model/orderDetails.model.js';
-import transaction from '../model/transactions.model.js';
+
 const initDB = async () => {
     try {
         await sequelize.authenticate();
---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import dotenv from 'dotenv';
+import initDB from './database/init.js';
+
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+// Register the routes
+
+
+// Initialize and synchronize the database
+initDB().then(() => {
+    app.listen(port, () => {
+        console.log(`Server running at http://localhost:${port}`);
+        // Initialize Swagger docs
+        // swaggerDocs(app, port);
+    });
+}).catch(error => {
+    console.error('Invalid database connection:', error);
+});
+
+// Handle graceful shutdown
+process.on('SIGINT', () => {
+    console.log('Closing PostgreSQL connection');
+    process.exit();
+});
