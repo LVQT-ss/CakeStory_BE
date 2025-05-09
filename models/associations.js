288b5d809e0f4db6ecd5668e82f2f2f4ca870dea
LVQT <levietquocthinh@gmail.com>
2025-05-09 17:36:31 +0700

    update model

--- models/associations.js
+++ models/associations.js
@@ -1,40 +1,7 @@
-import User from './user.model.js';
-import Role from './role.model.js';
-import UserRole from './userRole.model.js';
+
 
 function setupAssociations() {
-    // User-Role: Nhiều-nhiều thông qua UserRole
-    User.belongsToMany(Role, {
-        through: UserRole,
-        foreignKey: 'userId',
-        otherKey: 'roleId',
-        as: 'roles'
-    });
 
-    Role.belongsToMany(User, {
-        through: UserRole,
-        foreignKey: 'roleId',
-        otherKey: 'userId',
-        as: 'users'
-    });
-
-    // UserRole quan hệ một-nhiều với User (người được gán quyền)
-    UserRole.belongsTo(User, {
-        foreignKey: 'userId',
-        as: 'user'
-    });
-
-    // UserRole quan hệ một-nhiều với Role
-    UserRole.belongsTo(Role, {
-        foreignKey: 'roleId',
-        as: 'role'
-    });
-
-    // UserRole quan hệ một-nhiều với User (người gán quyền)
-    UserRole.belongsTo(User, {
-        foreignKey: 'assignedBy',
-        as: 'assigner'
-    });
 }
 
 export default setupAssociations;
