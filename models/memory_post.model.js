8f6d688ba4eebf2c0bcb3797156c384374d7d42e
LukeDragnar <trandainghia291203@gmail.com>
2025-05-29 16:30:26 +0700

    nghia - cập nhật lại memory post model

--- models/memory_post.model.js
+++ models/memory_post.model.js
@@ -3,7 +3,6 @@ import sequelize from '../database/db.js';
 
 const MemoryPost = sequelize.define('MemoryPost', {
   post_id: { type: DataTypes.INTEGER, primaryKey: true },
-  album_id: { type: DataTypes.INTEGER },
   event_date: { type: DataTypes.DATE },
   event_type: { type: DataTypes.STRING(100) },
   user_id: { type: DataTypes.INTEGER, allowNull: false }
