e7ccb18d6a5eeb908b1627e05ab7eb696bfb118a
LukeDragnar <trandainghia291203@gmail.com>
2025-05-29 15:41:16 +0700

    nghia - challenge model được thêm

---
+++ models/challenge.model.js
@@ -0,0 +1,22 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../database/db.js';
+
+const Challenge = sequelize.define('Challenge', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  title: { type: DataTypes.STRING(255), allowNull: false },
+  description: { type: DataTypes.TEXT },
+  start_date: { type: DataTypes.DATE, allowNull: false },
+  end_date: { type: DataTypes.DATE, allowNull: false },
+  winning_challenge_id: { type: DataTypes.INTEGER },
+  prize_description: { type: DataTypes.TEXT },
+  status: {
+    type: DataTypes.ENUM("notStart", "onGoing", "ended")
+  },
+  created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
+}, {
+  tableName: 'challenge',
+  timestamps: false,
+  underscored: true
+});
+
+export default Challenge;
