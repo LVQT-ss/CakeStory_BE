81aaa3e9646470180ccc5d15a1b963f4880767b5
LukeDragnar <trandainghia291203@gmail.com>
2025-05-29 15:34:33 +0700

    nghia - thêm marketplace post model

---
+++ models/marketplace_post.model.js
@@ -0,0 +1,18 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../database/db.js';
+
+const MarketplacePost = sequelize.define('MarketplacePost', {
+  post_id: { type: DataTypes.INTEGER, primaryKey: true },
+  shop_id: { type: DataTypes.INTEGER, allowNull: false },
+  user_id: { type: DataTypes.INTEGER, allowNull: false },
+  price: { type: DataTypes.INTEGER },
+  available: { type: DataTypes.BOOLEAN, defaultValue: true },
+  expiry_date: { type: DataTypes.DATE },
+  created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
+}, {
+  tableName: 'marketplace_post',
+  timestamps: false,
+  underscored: true
+});
+
+export default MarketplacePost;
