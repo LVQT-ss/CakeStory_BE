1821d82721d6e910af18c5586eedccb9e2566e84
LVQT <levietquocthinh@gmail.com>
2025-05-29 16:40:26 +0700

    update post data attribute

--- models/post_data.model.js
+++ models/post_data.model.js
@@ -5,8 +5,7 @@ const PostData = sequelize.define('PostData', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   post_id: { type: DataTypes.INTEGER, allowNull: false },
   image_url: { type: DataTypes.STRING(255) },
-  video_url: { type: DataTypes.STRING(255) },
-  caption: { type: DataTypes.STRING(255) }
+  video_url: { type: DataTypes.STRING(255) }
 }, {
   tableName: 'post_data',
   timestamps: false,
