258a574f637ab9efe604d4fda11879630ddc7bfa
LukeDragnar <trandainghia291203@gmail.com>
2025-06-18 10:35:42 +0700

    nghia - cập nhật lại thêm shop id cho shop model

--- models/shop.model.js
+++ models/shop.model.js
@@ -1,55 +1,37 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../database/db.js';
 
-const shop = sequelize.define('shop', {
-    user_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        references: {
-            model: 'user',
-            key: 'id'
-        }
-    },
-    business_name: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
-    },
-    business_address: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-    },
-    phone_number: {
-        type: DataTypes.STRING(50),
-        allowNull: true,
-    },
-    specialty: {
-        type: DataTypes.STRING(255),
-        allowNull: true,
-    },
-    bio: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-    },
-    
-    is_active: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: false,
-    },
-    longtitue: {
-        type: DataTypes.FLOAT,
-        allowNull:false,
-    },
-    latitude: {
-        type: DataTypes.FLOAT,
-        allowNull:false,
-    },
+const Shop = sequelize.define('shop', {
+  shop_id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'user',
+      key: 'id'
+    }
+  },
+  business_name: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  business_address: DataTypes.TEXT,
+  phone_number: DataTypes.STRING(20),
+  specialty: DataTypes.STRING(255),
+  bio: DataTypes.TEXT,
+  is_active: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
+  },
+  longitude: DataTypes.FLOAT,
+  latitude: DataTypes.FLOAT
 }, {
-    tableName: 'shop',
-    timestamps: false,
-    underscored: true,
+  tableName: 'shop',
+  timestamps: false
 });
 
-
-
-export default shop; 
+export default Shop;
