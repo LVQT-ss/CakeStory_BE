66f9f85d3f8807e1a860e967e6a9c31f81cc6821
LVQT <levietquocthinh@gmail.com>
2025-07-19 10:25:46 +0700

    [add] : integrate album routes and controller, update post model for album type

---
+++ routes/album.route.js
@@ -0,0 +1,9 @@
+import express from 'express';
+// import { createAlbum, createAlbumPost } from '../controllers/album.controller.js';
+import { verifyToken } from '../middleware/verifyUser.js';
+
+const router = express.Router();
+
+
+
+export default router;
