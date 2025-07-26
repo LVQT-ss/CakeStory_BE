985dc67bb06bd58de4c4b7b06c683805c1c5c9ec
LukeDragnar <trandainghia291203@gmail.com>
2025-07-26 21:58:26 +0700

    nghia - chỉnh sửa lại model của ingredient

--- models/Ingredient.model.js
+++ models/Ingredient.model.js
@@ -2,22 +2,14 @@ import { DataTypes } from 'sequelize';
 import sequelize from '../database/db.js';
 
 const Ingredient = sequelize.define('Ingredient', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  marketplace_post_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  name: {
-    type: DataTypes.STRING(255),
-    allowNull: false
-  },
-  price: {
-    type: DataTypes.DECIMAL(10, 2),
-    allowNull: false
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  shop_id: { type: DataTypes.INTEGER, allowNull: false },
+  name: { type: DataTypes.STRING(255), allowNull: false },
+  price: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
+  is_deleted: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
   }
 }, {
   tableName: 'ingredient',
