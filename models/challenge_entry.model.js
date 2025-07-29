b13de23c7eb4b9ccae1f33c715758a61b45b95b5
LukeDragnar <trandainghia291203@gmail.com>
2025-07-29 16:11:23 +0700

    nghia - update challenge entry model

--- models/challenge_entry.model.js
+++ models/challenge_entry.model.js
@@ -4,9 +4,7 @@ import sequelize from '../database/db.js';
 const ChallengeEntry = sequelize.define('ChallengeEntry', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   challenge_id: { type: DataTypes.INTEGER, allowNull: false },
-  user_id: { type: DataTypes.INTEGER, allowNull: false },
-  post_count: { type: DataTypes.INTEGER },
-  user_count: { type: DataTypes.INTEGER }
+  user_id: { type: DataTypes.INTEGER, allowNull: false }
 }, {
   tableName: 'challenge_entry',
   timestamps: false,
