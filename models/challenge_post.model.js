b0b85092ec9bbe1d024168c17acfd61d32a47525
LukeDragnar <trandainghia291203@gmail.com>
2025-07-31 22:45:32 +0700

    nghia - challenge post model update

--- models/challenge_post.model.js
+++ models/challenge_post.model.js
@@ -6,13 +6,18 @@ const ChallengePost = sequelize.define('ChallengePost', {
   post_id: { type: DataTypes.INTEGER, primaryKey: true },
   challenge_id: { type: DataTypes.INTEGER },
   challengeName: { type: DataTypes.STRING(100) },
+  user_id: { type: DataTypes.INTEGER, allowNull: false },
   is_challenge: { type: DataTypes.BOOLEAN, defaultValue: false },
   is_design: { type: DataTypes.BOOLEAN, defaultValue: false },
-  created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
+  is_active: { type: DataTypes.BOOLEAN, defaultValue: true },
+  created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+  deleted_at: { type: DataTypes.DATE, allowNull: true }
 }, {
   tableName: 'challenge_post',
   timestamps: false,
-  underscored: true
+  underscored: true,
+  paranoid: true,
+  deletedAt: 'deleted_at'
 });
 
 export default ChallengePost;
