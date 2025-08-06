5cabd3ba1487672a3ef6467f17d8c59d967cdc2c
LVQT <levietquocthinh@gmail.com>
2025-08-06 21:31:12 +0700

    [fix] model deletion and modify cake design attribute

--- models/cake_design.model.js
+++ models/cake_design.model.js
@@ -5,9 +5,8 @@ const CakeDesign = sequelize.define('CakeDesign', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   user_id: { type: DataTypes.INTEGER, allowNull: false },
   shop_id: { type: DataTypes.INTEGER, allowNull: false },
-  design_name: { type: DataTypes.STRING(255), allowNull: false },
   description: { type: DataTypes.TEXT },
-  design_data: { type: DataTypes.TEXT, allowNull: false },
+  design_image: { type: DataTypes.STRING },
   created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
   is_public: { type: DataTypes.BOOLEAN, defaultValue: true },
   is_ai_generated: { type: DataTypes.BOOLEAN, defaultValue: false }
