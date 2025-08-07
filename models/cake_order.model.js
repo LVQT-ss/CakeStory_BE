79297981d9ba536c1348f26126b161e1e3618c41
LukeDragnar <trandainghia291203@gmail.com>
2025-08-07 15:45:08 +0700

    nghia - update size for cake order

--- models/cake_order.model.js
+++ models/cake_order.model.js
@@ -8,6 +8,7 @@ const CakeOrder = sequelize.define('CakeOrder', {
   marketplace_post_id: { type: DataTypes.INTEGER, allowNull: true },
   base_price: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
   ingredient_total: { type: DataTypes.DECIMAL(10, 2), allowNull: true, defaultValue: 0.00 },
+  size: { type: DataTypes.STRING, allowNull: true },
   total_price: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
   status: {
     type: DataTypes.ENUM('pending', 'ordered', 'completed','cancelled', 'shipped'),
