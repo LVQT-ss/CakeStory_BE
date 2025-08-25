f24aee1b889041cc11ba37d3afaf92269c48899f
LukeDragnar <trandainghia291203@gmail.com>
2025-08-25 16:19:04 +0700

    nghia - fix model cake size

--- models/cake_size.model.js
+++ models/cake_size.model.js
@@ -1,9 +1,16 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../database/db.js';
+import MarketplacePost from './marketplace_post.model.js';
 
 const CakeSize = sequelize.define('CakeSize', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  marketplace_post_id: { type: DataTypes.INTEGER, allowNull: false },
+  marketplace_post_id: { 
+    type: DataTypes.INTEGER, 
+    allowNull: false,
+    references: { model: MarketplacePost, key: 'post_id' },
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE"
+  },
   size: { type: DataTypes.STRING, allowNull: false }, 
   price: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
 }, {
