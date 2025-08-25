8f6455d231fd9b2f97ab3854b4da61baf1570b03
LukeDragnar <trandainghia291203@gmail.com>
2025-08-25 16:19:24 +0700

    nghia - fix model challenge post

--- models/challenge_post.model.js
+++ models/challenge_post.model.js
@@ -1,15 +1,42 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../database/db.js';
+import Post from './post.model.js';
+import Challenge from './challenge.model.js';
+import User from './user.model.js';
 
 const ChallengePost = sequelize.define('ChallengePost', {
-  id: { type: DataTypes.INTEGER },
-  post_id: { type: DataTypes.INTEGER, primaryKey: true },
-  challenge_id: { type: DataTypes.INTEGER },
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+
+  post_id: { 
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: { model: 'post', key: 'id' },
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
+  },
+
+  challenge_id: { 
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: { model: 'challenge', key: 'id' },
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
+  },
+
+  user_id: { 
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: { model: 'user', key: 'id' },
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
+  },
+
   challengeName: { type: DataTypes.STRING(100) },
-  user_id: { type: DataTypes.INTEGER, allowNull: false },
+
   is_challenge: { type: DataTypes.BOOLEAN, defaultValue: false },
   is_design: { type: DataTypes.BOOLEAN, defaultValue: false },
   is_active: { type: DataTypes.BOOLEAN, defaultValue: true },
+
   created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
   deleted_at: { type: DataTypes.DATE, allowNull: true }
 }, {
