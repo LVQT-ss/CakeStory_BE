8ab99d9a6b0543adcea3d4b66cffbbd8de8307f3
LVQT <levietquocthinh@gmail.com>
2025-08-25 16:34:49 +0700

    thinh - fix memory post model

--- models/memory_post.model.js
+++ models/memory_post.model.js
@@ -1,11 +1,21 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../database/db.js';
+import User from './User.model.js';
 
 const MemoryPost = sequelize.define('MemoryPost', {
   post_id: { type: DataTypes.INTEGER, primaryKey: true },
   event_date: { type: DataTypes.DATE },
   event_type: { type: DataTypes.STRING(100) },
-  user_id: { type: DataTypes.INTEGER, allowNull: false }
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: User,
+      key: 'id'
+    },
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE"
+  }
 }, {
   tableName: 'memory_post',
   timestamps: false,
