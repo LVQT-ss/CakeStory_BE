9167faca23ee690b8fcaae61d027d9b6e6994769
LukeDragnar <trandainghia291203@gmail.com>
2025-08-25 16:18:54 +0700

    nghia - fix model cake order

--- models/cake_order.model.js
+++ models/cake_order.model.js
@@ -1,15 +1,36 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../database/db.js';
+import User from './user.model.js';
+import Shop from './shop.model.js';
+import MarketplacePost from './marketplace_post.model.js';
 
 const CakeOrder = sequelize.define('CakeOrder', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  customer_id: { type: DataTypes.INTEGER, allowNull: false },
-  shop_id: { type: DataTypes.INTEGER, allowNull: false },
-  marketplace_post_id: { type: DataTypes.INTEGER, allowNull: true },
+  customer_id: { 
+    type: DataTypes.INTEGER, 
+    allowNull: false,
+    references: { model: User, key: 'id' },
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE"
+  },
+  shop_id: { 
+    type: DataTypes.INTEGER, 
+    allowNull: false,
+    references: { model: Shop, key: 'shop_id' },
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE"
+  },
+  marketplace_post_id: { 
+    type: DataTypes.INTEGER, 
+    allowNull: true,
+    references: { model: MarketplacePost, key: 'post_id' },
+    onDelete: "SET NULL",
+    onUpdate: "CASCADE"
+  },
   base_price: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
   ingredient_total: { type: DataTypes.DECIMAL(10, 2), allowNull: true, defaultValue: 0.00 },
   size: { type: DataTypes.STRING, allowNull: true },
-  tier: { type: DataTypes.INTEGER, allowNull: true, },
+  tier: { type: DataTypes.INTEGER, allowNull: true },
   total_price: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
   status: {
     type: DataTypes.ENUM('pending', 'ordered', 'completed','cancelled', 'shipped', 'complaining', 'prepared'),
